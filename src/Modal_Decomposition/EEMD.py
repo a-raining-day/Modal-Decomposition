4564f9db010e89a3279a59cde1c05b6a500880dd
Kolita-Huccika <2215269365@qq.com>
2026-03-27 09:07:31 +0800

    2026.3.27 - Change the EEMD class' usage. EEMD(parallel=True) (default) -> EEMD(parallel=False). Now, it will not use parallel default.

--- src/Modal_Decomposition/EEMD.py
+++ src/Modal_Decomposition/EEMD.py
@@ -8,11 +8,12 @@ Lib and Version:  (if None write None)
 Only accessed by:  (must)
     Only __init__.py
 
-Modify:  (must)
-    2026.3.25
-
 Description: (if None write None)
     Realize the EEMD.
+
+Modify:  (must)
+    2026.3.25 - I don't know, because when I'm find I don't write this line.
+    2026.3.27 - Change the EEMD class' usage. EEMD(parallel=True) (default) -> EEMD(parallel=False). Now, it will not use parallel default.
 """
 import numpy as np
 from PyEMD import EEMD
@@ -20,7 +21,7 @@ from typing import Union, Tuple
 
 Origin_EEMD = EEMD
 
-EEMD = EEMD()
+EEMD = EEMD(parallel=False)
 def eemd(S: Union[list, np.ndarray]) -> Tuple[np.ndarray, np.ndarray]:
     """
     :param S: Signal (1-dim)

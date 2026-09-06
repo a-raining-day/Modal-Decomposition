4011a2c9047ae4675724c59e7a64a220e500d26f
Kolita-Huccika <2215269365@qq.com>
2026-09-06 15:34:10 +0800

    2026.9.6  - Starting optimize the hilbert method.

---
+++ src/Modal_Decomposition/Utils/Hilbert/__init__.py
@@ -0,0 +1,29 @@
+import numpy as np
+from typing import Literal
+
+from . import (
+    _fft,
+    _fht,
+    _fir,
+    _kramers_kronig,
+    _scipy_backend,
+    _wave
+)
+
+def hilbert \
+(
+    S: np.ndarray,
+    mod: Literal \
+    ["Scipy", "Fourth-order Wave", "FHT", "SB-Hilbert", "Kramers-Kronig", "FIR", "HST", "FFT", "Optional-order FIR"]
+) -> np.ndarray:
+
+    match mod:
+        case "Scipy":
+            return _scipy_backend._hilbert(S)
+
+        case "Fourth-order Wave":
+            ...
+
+        case "FHT":
+            ...
+
---
+++ src/Modal_Decomposition/Utils/Hilbert/_fht.py
@@ -0,0 +1,3 @@
+import numpy as np
+
+def _hilbert(S: np.ndarray) -> np.ndarray:
